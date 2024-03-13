f09ed32aefe827e31186021fb7c911ce2b7d821f
marcotozzy <sergey.erokhin2003@bk.ru>
2024-03-13 20:51:55 +0700

    add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,82 @@
+import React from "react"
+import './Footer.css'
+
+const Footer = () =>{
+    return(
+        <div className="Footer-main">
+                 <div className="Footer-img"><img className="Footer-logo" src="https://s3-alpha-sig.figma.com/img/8354/c0a8/579180fd272afd63598cafc72d2f6cc3?Expires=1710720000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ocYY6jMzDuHQrmgMeIfGWcP9XnwZW4qslBwUYJl8GH17HlVQjIA3FE3dGfyBcbO7KHC3JOwp~7i-j5Kj3uQBa1shZbp4jZIEkfoFNaAIdvFXdu4Pbxl~c7V~clz~vQwvh85ediGm1Ig~BZlL9vsif2G7cG4IqGZ1hhB~sbijN5rcBRTN5RIwOglZpPcvYNY1CB3Zm0kE2TsuZeTKLNPleuO0dbIbe1K76v5QN~bwP5R3bHlicbq~EIPw59BW5QwQhUySKU~1Fz8SrOITnRcn9b8noxow1zsumLEP8uyX2ldlyl3QRwUdgVqHWNSfXJiaI21af5Rhn89hpvrAeggE4A__" alt="" /></div>    
+            <div className="Footer-wrapper">
+                <div className="Footer-item">
+                    <div className="Footer-item-maintext"> <h3>О ПРОЕКТЕ</h3> </div>
+                    <div className="Footer-item-secondtext">
+                        <li> <a href="#">Отзывы о CodeLab</a> </li>
+                        <li> <a href="#">Политика конфиденциальности</a> </li>
+                        <li> <a href="#">Контакты</a> </li>
+                        <li> <a href="#">Лицензионное соглашение</a> </li>
+                        <li> <a href="#">Карта сайта</a> </li>
+                    </div>
+                </div>
+                <div className="Footer-item">
+                    <div className="Footer-item-maintext"> <h3>НАУКА</h3> </div>
+                    <div className="Footer-item-secondtext">
+                        <li> <a href="#">Методики</a> </li>
+                        <li> <a href="#">Как работает CodeLab</a> </li>
+                        <li> <a href="#">Курсы</a> </li>
+                        <li> <a href="#">Тренажеры</a> </li>
+
+                    </div>
+
+                </div>
+                <div className="Footer-item">
+                    <div className="Footer-item-maintext"> <h3>СОТРУДНИЧЕСТВО</h3> </div>
+                    <div className="Footer-item-secondtext">
+                        <li> <a href="#">Партнерская программа</a> </li>
+                        <li> <a href="#">Вакансии</a> </li>
+                        <li> <a href="#">Для организаций</a> </li>
+
+                    </div>
+                </div>
+                <div className="Footer-item">
+                    <div className="Footer-item-maintext"> <h3>СОЦИАЛЬНЫЕ СЕТИ</h3> </div>
+                    <div className="Footer-item-massengers">
+                    
+                        <div className="Footer-item-massengers-logo"><img src="https://img.icons8.com/?size=24&id=DpOQ6G5p47f0&format=png" alt=""/></div>
+                        <div className="Footer-item-massengers-logo"><img src="https://img.icons8.com/?size=24&id=85088&format=png" alt="" /></div>
+                        <div className="Footer-item-massengers-logo"><img src="https://img.icons8.com/?size=24&id=86124&format=png" alt="" /></div>
+                        <div className="Footer-item-massengers-logo"><img src="https://img.icons8.com/?size=24&id=85466&format=png" alt="" /></div>
+
+                    </div>
+                </div>
+                <div className="Footer-item">
+                    <div className="Footer-item-maintext"> <h3>ДРУГОЕ</h3> </div>
+                    <div className="Footer-item-secondtext">
+                        <li> <a href="#">Помощь (FAQ)</a> </li>
+                        <li> <a href="#">support@codelab.ru</a> </li>
+                        <li> <a href="#">+79539999999</a> </li>
+
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+        </div>
+    )
+}
+
+
+export default Footer
